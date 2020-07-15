ed7e2bee026832ea79f93a90ecca99ac70624ab9
LucasLeibs <dr.lucasleibs@gmail.com>
2020-07-14 18:21:59 -0700

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,12 @@
-## code your solution here. 
+## code your sol`ution here. 
+class Cat
+attr_accessor :name
+    def initialized(name, meow)
+    @name = name
+    @meow = meow
+end
+
+def meow
+    puts "meow!"
+end
+end
